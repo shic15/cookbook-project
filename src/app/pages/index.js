3e03aa82290cd496ba7ebec2becc9dd19af02cab
shic15 <agarwalshivanshu00@gmail.com>
2024-01-13 15:25:49 +0530

    [WIP]Init repository

---
+++ src/app/pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
